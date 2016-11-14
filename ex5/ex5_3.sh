86925b1604eb5e64bef90988d3044bb03c22e01c
wangjunwei <wangjunwei1266@126.com>
2016-11-14 15:26:18 +0800

    add ex5_3/4.sh

---
+++ ex5/ex5_3.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+#列出/ex5目录中所有ex5_..sh的目录
+str=`ls | grep "ex5_..sh"`
+echo "$str"
---
+++ ex5/ex5_4.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+#筛选t1.txt中aj行
+str=`cat /home/wangjunwei/shell/shellxuexi/test/t1.txt | grep "ajj*"`
+
+echo "$str"
